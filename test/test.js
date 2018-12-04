28771b38643acdc42fa9c72d1d8bca8f2ef9c0c5
jasonfutch <jasonfutch@Jasons-MBP-2.attlocal.net>
2018-12-04 10:53:55 -0500

    fix() - missing asserts

--- test/test.js
+++ test/test.js
@@ -77,18 +77,29 @@ describe('layerize', () => {
             it('should insert a single record', async () => {
 
                 let layers = layerize.layers({ schemaName: testSchemaName });
+                let beforeCount = await layers.count('users');
+
                 await layers.insert('user_role', { id: 'a8988288-988a-412a-9127-e51a284e2b46', name: 'Admin', permissions: {} });
-                return await layers.insert('users', { id: 'a99f0cea-c3df-4619-b023-8c71fee3a9cd', user_role_id: 'a8988288-988a-412a-9127-e51a284e2b46', first_name: 'John', last_name: ' Doe ', username: 'johndoe10', password: 'Mypassword1', email: 'pickle@dsfsd.com', system_keys: [ { key: '1', value: '2' } ], custom_fields: { pickle: true } });
+                await layers.insert('users', { id: 'a99f0cea-c3df-4619-b023-8c71fee3a9cd', user_role_id: 'a8988288-988a-412a-9127-e51a284e2b46', first_name: 'John', last_name: ' Doe ', username: 'johndoe10', password: 'Mypassword1', email: 'pickle@dsfsd.com', system_keys: [ { key: '1', value: '2' } ], custom_fields: { pickle: true } });
+
+                let afterCount = await layers.count('users');
+
+                assert.equal(true, (afterCount === beforeCount + 1));
 
             }).slow(500).timeout(15000);
 
             it('should insert multiple records', async () => {
 
                 let layers = layerize.layers({ schemaName: testSchemaName });
-                return await layers.insertMany('users', [
+                let beforeCount = await layers.count('users');
+
+                await layers.insertMany('users', [
                     { id: 'b99f0cea-c3df-4619-b023-8c71fee3a9dc', user_role_id: 'a8988288-988a-412a-9127-e51a284e2b46', first_name: 'Mary', last_name: ' Doe ', username: 'marydoe', password: 'Mypassword1', email: 'mary@doe.com', system_keys: [ { key: '1', value: '2' } ], custom_fields: { pickle: true } },
                     { user_role_id: 'a8988288-988a-412a-9127-e51a284e2b46', first_name: 'Jane', last_name: ' Doe ', username: 'janedoe', password: 'Mypassword1', email: 'jane@doe.com', system_keys: [ { key: '1', value: '2' } ], custom_fields: { pickle: true } }
                 ]);
+                let afterCount = await layers.count('users');
+
+                assert.equal(true, (afterCount === beforeCount + 2));
 
             }).slow(500).timeout(15000);
 
